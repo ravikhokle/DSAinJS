3ce93d0bd79e46acbd30f2bd8ba8e4e4bac74973
ravikhokle <ravikhokle1@gmail.com>
2025-05-25 21:56:59 +0530

    sum questions & coersion

---
+++ sum.js
@@ -0,0 +1,16 @@
+
+// strings and numbers in JavaScript
+let a = 10;
+let b = 20;
+console.log("sum of 10 and 20 is: " + a + b); 
+console.log("sum of 10 and 20 is:, " + a + b); 
+console.log( a + b + " is sum of 10 and 20"); 
+console.log(10 + "10");
+
+// Type coercion examples
+console.log("1"+1); // "11"
+console.log("1"-1); // 0
+console.log("1"*1); // 1
+console.log("1"/1); // 1
+
+
