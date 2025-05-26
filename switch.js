dbee987ca0372918a3856fff7c78b72e919abd1e
ravikhokle <ravikhokle1@gmail.com>
2025-05-26 21:08:25 +0530

    switch

---
+++ switch.js
@@ -0,0 +1,26 @@
+switch(true){
+    case 20<10:
+        console.log("Hello");
+        break;
+    case 10>5:
+        console.log("Hey");
+        break;
+    default: console.log("invalid");
+}
+
+
+let day = 1;
+switch(day){
+    case 1:
+    case 2:
+    case 3:
+        console.log("Monday");
+        break;
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+        console.log("Sunday");
+        break;
+    default: console.log("invalid");
+}
