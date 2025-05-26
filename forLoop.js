455a6ded51d4e24ad44bf4d47b66c2a105f3b33d
ravikhokle <ravikhokle1@gmail.com>
2025-05-26 21:37:26 +0530

    loop & sum of n & factorial

---
+++ forLoop.js
@@ -0,0 +1,20 @@
+// for(;;){
+//     console.log("it is a valid loop, but infinite loop");
+// }
+
+
+// program for sum of N number
+let num=5;
+let sum=0;
+for(let i=1;i<=num;i++){
+    sum = sum+i;
+}
+console.log(sum);
+
+// program for factorial number
+let input=5;
+let fact=1;
+for(let i=1; i<=input; i++){
+    fact = fact*i;
+}
+console.log(fact);
